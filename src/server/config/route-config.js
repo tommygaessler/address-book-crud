9c867e42413882dfc234ad600de18d496f40234d
Tommy Gaessler <tommy.gaessler@gmail.com>
2016-09-19 12:03:30 -0600

    added get route and displayed contacts on page

--- src/server/config/route-config.js
+++ src/server/config/route-config.js
@@ -5,10 +5,10 @@
   routeConfig.init = function (app) {
 
     // *** routes *** //
-    const routes = require('../routes/index');
+    const index = require('../routes/index');
 
     // *** register routes *** //
-    app.use('/', routes);
+    app.use('/', index);
 
   };
 
